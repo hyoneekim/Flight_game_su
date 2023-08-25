d05c469e568f009e900050fe3e61d63fdf0060c7
hyoneelol <su.kim@metropolia.fi>
2023-08-25 11:25:49 +0300

    Initial Commit

--- w1_1.py
+++ w1_1.py
@@ -3,3 +3,7 @@ import math
 radius = float(input("Enter radius: "))
 areaCircle = math.pi * radius **2
 print(f"Are of circle {areaCircle:.4f}")
+
+#This is from class, commit testing
+name = input("Type your name: \n")
+print(f"Your name is: {name}")
