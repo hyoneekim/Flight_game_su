7749d954c6161f90404a948c8c5f86c97515b088
hyoneelol <su.kim@metropolia.fi>
2023-09-06 17:24:19 +0300

    Commit

---
+++ Excercises/module_5.py
@@ -0,0 +1,30 @@
+import random
+print("5_1\n")
+numbers = []
+howMany = 0
+sumOfNums = 0
+ask = int(input("How many dice to roll?: "))
+while (howMany < ask):
+    numbers.append(random.randint(1, 6))
+    howMany = howMany + 1
+
+print(numbers)
+for n in numbers:
+    sumOfNums = sumOfNums + n
+
+print(f"the sum of number is {sumOfNums}.")
+
+print("\n5_2\n")
+
+askNum = input("Enter a number or quit by enter space. ")
+receivedNum = []
+
+while(askNum != " "):
+    receivedNum.append(int(askNum))
+    askNum = input("Enter a number or quit by enter space. ")
+
+receivedNum.sort(reverse=True)
+print(f"The five greatest numbers are: \n {receivedNum[0:5]}")
+
+print("\n5_3\n")
+
