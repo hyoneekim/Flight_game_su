f565e3d684af5ff5e21857e3aeb83597d885d55a
hyoneelol <su.kim@metropolia.fi>
2023-09-07 15:55:24 +0300

    Commit

---
+++ Excercises/module_6.py
@@ -0,0 +1,37 @@
+print("\n6_1\n")
+
+import random
+
+def dice():
+    result = random.randint(1,6)
+    return result
+
+results = []
+count = 1
+
+result = dice()
+
+while (result != 6):
+    results.append(result)
+    print(f" {count} round's result: {result}")
+    count = count + 1
+    result = dice()
+
+print("\n6_2\n")
+
+def dice_custom(num):
+    result = random.randint(1,num)
+    return result
+
+numAsked = int(input("Enter the number of sides on the dice : "))
+
+results2 = []
+count2 = 1
+result = dice_custom(numAsked)
+
+
+while (result != numAsked):
+    results2.append(result)
+    print(f" {count2} round's result: {result}")
+    count2 = count2 + 1
+    result = dice_custom(numAsked)
