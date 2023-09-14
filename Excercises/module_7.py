c750a6640c4119b0b6dd3c2b51eb7dcad864b4da
hyoneelol <su.kim@metropolia.fi>
2023-09-14 14:47:49 +0300

    Commit

--- Excercises/module_7.py
+++ Excercises/module_7.py
@@ -25,3 +25,31 @@ askMonth = int(input("Enter a number of a month (1-12): "))
 print(f"{months[askMonth - 1]} is {month_to_season(askMonth)}.")
 
 print("\n\n<7_2>\n\n")
+#Write a program that asks the user to enter names until he/she enters an empty string.
+#After each name is read the program either prints out New name or Existing name
+# depending on whether the name was entered for the first time.
+# Finally, the program lists out the input names one by one, one below another in any order.
+# Use the set data structure to store the names.
+
+names = {"Antti", "John"}
+
+def is_name_in(x,y):
+    if (x in y):
+        print(f"The name '{x}' is already in the list.")
+    else:
+        y.add(x)
+        print(f"The name '{x}' has been added.")
+
+
+askName = input("Enter a name. Exit by hitting Enter key.: ")
+while (askName != ""):
+    is_name_in(askName, names)
+    askName = input("Enter a name. Exit by hitting Enter key.: ")
+if (askName == ""):
+    print("The listed names are: \n")
+    for x in names:
+        print(x)
+
+print("\n\n<7_3>\n\n")
+
+
