c17bec9413592629a0c55d7906fcbc8f39a9ce86
hyoneelol <su.kim@metropolia.fi>
2023-09-14 15:34:49 +0300

    Commit

--- Excercises/module_7.py
+++ Excercises/module_7.py
@@ -31,7 +31,7 @@ print("\n\n<7_2>\n\n")
 # Finally, the program lists out the input names one by one, one below another in any order.
 # Use the set data structure to store the names.
 
-names = {"Antti", "John"}
+names = set()
 
 def is_name_in(x,y):
     if (x in y):
@@ -51,5 +51,52 @@ if (askName == ""):
         print(x)
 
 print("\n\n<7_3>\n\n")
+# Write a program for fetching and storing airport data.
+# The program asks the user if they want to enter a new airport,
+# fetch the information of an existing airport or quit.
+# If the user chooses to enter a new airport,
+# the program asks the user to enter the ICAO code and name of the airport.
+# If the user chooses to fetch airport information instead,
+# the program asks for the ICAO code of the airport and prints out the corresponding name.
+# If the user chooses to quit, the program execution ends.
+# The user can choose a new option as many times they want until they choose to quit.
+# (The ICAO code is an identifier that is unique to each airport.
+# For example, the ICAO code of Helsinki-Vantaa Airport is EFHK.
+# You can easily find the ICAO codes of different airports online.)
+airports = {
+   "EFHK" : "Helsinki-Vantaa"
+}
+processing = True
+
+def add_airport (y):
+   ICAO = input("\nEnter the ICAO code of a new airport. : ")
+   name = input("\nWhat is the name of the new airport?. : ")
+   if (ICAO not in y):
+      airports[ICAO] = name
+      print("\nThe New airport has been added.")
+   else:
+      print("\nThe airport is already in the data.")
+
+def fetch_airport (y):
+   ICAO = input("\nEnter the ICAO code of an airport to fetch its data. : ")
+   if( ICAO in y):
+      print(f"\nName found: The code {ICAO} is {airports[ICAO]} airport.")
+   else:
+      print("\nThe code not found or incorrect input.")
 
 
+while processing:
+   print("\n\n1: Enter a new airport")
+   print("2: Fetch information of an existing airport")
+   print("3: Quit\n\n")
+
+   askCommand = int(input("Enter a number of your command. : "))
+   if (askCommand == 1):
+      add_airport(airports)
+   elif (askCommand == 2):
+      fetch_airport(airports)
+   elif (askCommand ==3):
+      processing = False
+      print("Goodbye.")
+   else:
+      print("\nInvalid input. Please enter again.")
