f2b0a396a60d14272157c8ee7b6cb9301edcb7e9
hyoneelol <su.kim@metropolia.fi>
2023-09-15 12:31:44 +0300

    Commit

--- Excercises/module_8.py
+++ Excercises/module_8.py
@@ -51,3 +51,44 @@ def getCountryAndTypeByIso_country(iso_country):
 iso_country = input("Enter an area code: ")
 getCountryAndTypeByIso_country(iso_country)
 
+
+print("\n\n<8_3>\n\n")
+
+# Write a program that asks the user to enter the ICAO codes of two airports.
+# The program prints out the distance between the two airports in kilometers.
+# The calculation is based on the airport coordinates fetched from the database.
+# Calculate the distance using the geopy library: https://geopy.readthedocs.io/en/stable/.
+# Install the library by selecting View / Tool Windows / Python Packages in your PyCharm IDE,
+# write geopy into the search field and finish the installation.
+
+def calculateDistance(ident1,ident2):
+    from geopy import distance
+
+    sql = "SELECT latitude_deg, longitude_deg, name FROM airport WHERE ident = '" + ident1 + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"The {row[2]} is located in ({row[0]},{row[1]}).")
+            lo1 = (row[0],row[1])
+
+    sql = "SELECT latitude_deg, longitude_deg, name FROM airport WHERE ident = '" + ident2 + "'"
+    print(sql)
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    if cursor.rowcount > 0:
+        for row in result:
+            print(f"The {row[2]} is located in ({row[0]},{row[1]}).")
+            lo2 =(row[0],row[1])
+    distanceKm = distance.distance(lo1,lo2).km
+    print(f"The distance between {ident1} and {ident2} is {distanceKm:.3f}Km.")
+
+
+
+airport1 = input("Enter the first airport's ICAO code: ") # EFHK
+airport2 = input("Enter the second airport's ICAO code: ") # RKSI
+
+calculateDistance(airport1,airport2)
