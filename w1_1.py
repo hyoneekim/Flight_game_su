f88f56c09fecee92d5091c83e2db266887a6af2f
hyoneekim <97020327+hyoneekim@users.noreply.github.com>
2023-10-03 17:18:20 +0300

    Delete w1_1.py

--- w1_1.py
+++
@@ -1,9 +0,0 @@
-import math
-
-radius = float(input("Enter radius: "))
-areaCircle = math.pi * radius **2
-print(f"Are of circle {areaCircle:.4f}")
-
-#This is from class, commit testing
-name = input("Type your name: \n")
-print(f"Your name is: {name}")
