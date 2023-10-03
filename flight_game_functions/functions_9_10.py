95f36ea14479ab3d6b9d8ff7895071bc8ca33489
hyoneelol <su.kim@metropolia.fi>
2023-10-03 16:32:07 +0300

    Commit

--- flight_game_functions/functions_9_10.py
+++ flight_game_functions/functions_9_10.py
@@ -21,8 +21,17 @@ def game_over(id):
             print(f"{row[0]}, You cannot fly with the remaining budget anywhere. GAME OVER!")
             name = row[0]
             score = row[1]
-        return
+    save = input("Do you want to save your score to the score board? (y/n): ")
+    if save == 'y':
+        sql2 = f"INSERT INTO scoreboard (player_name, score) VALUES (%s, %s)"
+        val = [name, score]
+        cursor = connection.cursor()
+        cursor.execute(sql2, val)
+        print(f"{name} and {score} has been updated to the scoreboard.")
+    return
+
 
 # for test
 test1 = int(input("player id?: "))
 game_over(test1)
+
