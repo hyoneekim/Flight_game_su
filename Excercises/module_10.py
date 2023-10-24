b2a612e851b8088ea4d805a6177dbbb8bdea212d
hyoneelol <su.kim@metropolia.fi>
2023-10-24 20:37:22 +0300

    Commit

--- Excercises/module_10.py
+++ Excercises/module_10.py
@@ -1,3 +1,4 @@
+import random
 print("<10_1>")
 
 class Elevator:
@@ -49,11 +50,13 @@ class Building:
             self.elevators.append(i)
 
     def run_elevator(self, des):
+        print(f"Moving all the elevators to the {des} floor..")
         for index, ele in enumerate(self.elevators, start = 1):
             print(f"<Elevator No.{index}>")
             ele.go_to_floor(des)
 
     def fire_alarm(self):
+        print("Fire alarm has been set off. Moving all the elevators to the bottom floor..")
         for index, ele in enumerate(self.elevators, start = 1):
             print(f"<Elevator No.{index}>")
             ele.go_to_floor(self.bottom)
@@ -62,9 +65,59 @@ class Building:
 
 b = Building(-3,13,3)
 b.run_elevator(3)
+b.run_elevator(11)
 
 print("\n<10_3>\n")
 
 b.fire_alarm()
 
 print("\n<10_4>\n")
+
+class Car:
+
+    def __init__(self, n, ms, cs=0, td=0):
+        self.num = n
+        self.max_speed = ms
+        self.current_speed = cs
+        self.travelled_distance = td
+
+    def drive(self, hours =1):
+        self.travelled_distance += self.current_speed * hours
+
+    def accelerate(self):
+        speed = random.randint(-10,15)
+        self.current_speed += speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed - 1
+            
+class Race:
+    def __init__(self, n, d):
+        self.name = n
+        self.distance = d
+        self.cars = []
+        for i in range(10):
+            i = Car(f"ABC-{i + 1}", random.randint(100, 200))
+            self.cars.append(i)
+
+    def hour_passes(self):
+        hours = 1
+        for car in self.cars:
+            car.accelerate()
+            car.drive()
+        hours += 1
+
+    def print_status(self):
+        for car in self.cars:
+            print(f"car {car.num}: Maximum speed: {car.max_speed}km/h, Current speed: {car.current_speed}km/h, Travelled distance: {car.travelled_distance}km")
+
+    def race_finished(self):
+        for car in self.cars:
+            if car.travelled_distance > self.distance:
+             return True
+
+r = Race("Grand Demolition Derby", 8000)
+
+while False:
+    r.hour_passes()
