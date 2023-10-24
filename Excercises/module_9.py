b4ec08988fd468c44feb29638130bf06923729af
hyoneelol <su.kim@metropolia.fi>
2023-10-24 16:33:54 +0300

    Commit

--- Excercises/module_9.py
+++ Excercises/module_9.py
@@ -1,6 +1,5 @@
+import random
 print("<9-1>\n")
-
-
 
 class Car:
 
@@ -17,8 +16,17 @@ class Car:
         elif self.current_speed > self.max_speed:
             self.current_speed = self.max_speed -1
 
-    def drive(self, hours):
+    def drive(self, hours =1):
         self.travelled_distance += self.current_speed * hours
+
+    def accelerate(self):
+        speed = random.randint(-10,15)
+        self.current_speed += speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed - 1
+
 
 
 
@@ -40,13 +48,26 @@ c1.accel(-200)
 print(f"Emergency Break! Your current speed: {c1.current_speed}, Travelled distance: {c1.travelled_distance}km")
 
 print("\n<9-4>\n")
-import random
 cars = []
 for i in range(10):
     i = Car(f"ABC-{i+1}", random.randint(100,200))
     cars.append(i)
-    print(
-        f"Registration plate: {i.num}, Maximum speed: {i.max_speed}km/h, Current speed: {i.current_speed}km/h, Travelled distance: {i.travelled_distance}km")
+    #print(
+    #    f"Registration plate: {i.num}, Maximum speed: {i.max_speed}km/h, Current speed: {i.current_speed}km/h, Travelled distance: {i.travelled_distance}km")
+
+hour = 1
+racing = True
+while racing:
+    print(f"\nResult after {hour}hours:\n")
+    for car in cars:
+        car.accelerate()
+        car.drive()
+        print(f"car {car.num}: Maximum speed: {car.max_speed}km/h, Current speed: {car.current_speed}km/h, Travelled distance: {car.travelled_distance}km")
+        if car.travelled_distance > 10000:
+            racing = False
+        hour += 1
+
+
 
 
 
