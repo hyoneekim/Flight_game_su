e19dea6dc9928da72e6c705f6b63bfeda3d34d76
hyoneelol <su.kim@metropolia.fi>
2023-10-25 10:17:44 +0300

    Commit

--- Excercises/module_11.py
+++ Excercises/module_11.py
@@ -1,3 +1,4 @@
+import random
 print("<11_1>\n")
 
 class Publication:
@@ -32,3 +33,36 @@ p.append( Magazine("Donald Duck", "Aki Hyyppä"))
 p.append( Book("Compartment No. 6", "Rosa Liksom", 192))
 for i in p:
     i.print_information()
+
+
+print("\n<11_2>\n")
+
+class Car:
+
+    def __init__(self, n, ms, cs=0, td=0):
+        self.num = n
+        self.max_speed = ms
+        self.current_speed = cs
+        self.travelled_distance = td
+
+    def drive(self, hours =1):
+        self.travelled_distance += self.current_speed * hours
+
+    def accelerate(self):
+        speed = random.randint(-10,15)
+        self.current_speed += speed
+        if self.current_speed < 0:
+            self.current_speed = 0
+        elif self.current_speed > self.max_speed:
+            self.current_speed = self.max_speed - 1
+
+class ElectricCar(Car):
+    def __init__(self, n, ms, cap):
+        super().__init__(n, ms)
+        self.capacity = cap
+
+class GasolineCar(Car):
+    def __init__(self, n, ms, vol):
+        super().__init__(n, ms)
+        self.volume = vol
+
